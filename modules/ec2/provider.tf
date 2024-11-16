91c54d7f8578d918bc5da50210020e832bd25093
pkkumar1234 <pk505109@gmail.com>
2024-11-16 22:59:46 +0530

    Update provider.tf

--- modules/ec2/provider.tf
+++ modules/ec2/provider.tf
@@ -1,12 +1,1 @@
-variable "aws_ami_id" {
-  description = "server ami id"
-  type = string
-}
-variable "aws_instance_type" {
-  description = "aws instance type"
-  type = string
-}
-variable "aws_instance_name" {
-  description = "aws instance name"
-  type = string
-}
+
